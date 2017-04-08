867bc634bf082f8dc56ffea675d971d315633bc0
Vincent Yang <VincentYang2014@gmail.com>
2017-04-08 10:42:21 -0700

    Formspree

--- php/send.php
+++ php/send.php
@@ -4,7 +4,7 @@ $email = $_POST['email'];
 $message = $_POST['message'];
 $subject = $_POST['subject'];
 
-$to = 'hello@gmail.com';
+$to = 'contactbcnc@gmail.com';
 $message = 'FROM: '.$name.' Email: '.$email.'Message: '.$message;
 $headers = 'From: youremail@domain.com' . "\r\n";
  
